7b285298f61f3b832e53427ad68928f8fea7e2f3
MANI <manigupta.mg21@gmail.com>
2024-08-23 14:23:22 +0530

    Remove node_modules from repository and add to .gitignore

--- index.js
+++ index.js
@@ -1,10 +1,105 @@
+// let con = require('./connection');
+// let express = require("express");
+// let app = express();
+// let path = require("path");
+// let bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}))
+// app.use(express.static(__dirname));
+
+// con.connect(function(error) {
+//     if (error) throw error;
+//     console.log("Connected to the database.");
+// });
+
+// app.set('view engine','ejs');
+
+
+// app.get("/register", function(req, res) {
+//     res.sendFile(path.join(__dirname, "index.html"));
+//     console.log("Started");
+// });
+// app.post("/register",function(req,res){
+//  let name = req.body.name;
+//  let email = req.body.email;
+//  let contact = req.body.contact;
+//  //con.connect(function(error){                  shows enqueue error 
+//  //   if (error) throw error
+
+//     let sql = "INSERT INTO details (name, email, contact) VALUES (?, ?, ?)";
+//         con.query(sql, [name, email, contact], function (error, result) {
+//             if (error) throw error;
+//             // res.send('Student successfully registered');
+//             res.redirect('/student');
+//     })
+//  })
+// //})
+
+// app.get('/student',function(req,res){
+   
+
+//         let sql = 'select * from details';
+//         con.query(sql,function(error,result){
+//             if (error) throw error
+
+//             res.render(__dirname+"/student",{student:result});
+
+
+//         })
+//     })
+
+// app.get('/delete-student',function(req,res){
+//     let sql = "delete from details where id=?"
+//     let id = req.query.id;
+//     con.query(sql,[id],function(error,result){
+//         if (error) throw error
+
+//         res.redirect('/student');
+
+
+//     })
+    
+// })
+
+
+// app.get('/update-student',function(req,res){
+//     let sql = "select * from details where id=?"
+//     let id = req.query.id;
+//     con.query(sql,[id],function(error,result){
+//         if (error) throw error
+//         res.render(__dirname+ "/update-student",{student:result})
+        
+
+
+//     })
+    
+// })
+
+// app.post("/update-student",function(req,res){
+//     let name = req.body.name;
+//     let email = req.body.email;
+//     let contact = req.body.contact;
+//     let id = req.body.id;
+//     //con.connect(function(error){                  shows enqueue error 
+//     //   if (error) throw error
+   
+//        let sql = "UPDATE details set name=?,email=?,contact=? where id = ?";
+//            con.query(sql, [name, email, contact,id], function (error, result) {
+//                if (error) throw error;
+//                // res.send('Student successfully registered');
+//                res.redirect('/student');
+//        })
+//     })
+
+
 let con = require('./connection');
 let express = require("express");
 let app = express();
 let path = require("path");
 let bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname));
 
 con.connect(function(error) {
@@ -14,96 +109,71 @@ con.connect(function(error) {
 
 app.set('view engine','ejs');
 
-
 app.get("/register", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
     console.log("Started");
 });
+
 app.post("/register",function(req,res){
- let name = req.body.name;
- let email = req.body.email;
- let contact = req.body.contact;
- //con.connect(function(error){                  shows enqueue error 
- //   if (error) throw error
+    let name = req.body.name;
+    let email = req.body.email;
+    let contact = req.body.contact;
 
     let sql = "INSERT INTO details (name, email, contact) VALUES (?, ?, ?)";
-        con.query(sql, [name, email, contact], function (error, result) {
-            if (error) throw error;
-            // res.send('Student successfully registered');
-            res.redirect('/student');
-    })
- })
-//})
+    con.query(sql, [name, email, contact], function (error, result) {
+        if (error) throw error;
+        res.redirect('/student');
+    });
+});
 
 app.get('/student',function(req,res){
-   
+    let sql = 'SELECT * FROM details';
+    con.query(sql, function(error, result){
+        if (error) throw error;
+        res.render(__dirname + "/student", {student: result});
+    });
+});
 
-        let sql = 'select * from details';
-        con.query(sql,function(error,result){
-            if (error) throw error
-
-            res.render(__dirname+"/student",{student:result});
-
-
-        })
-    })
-
-app.get('/delete-student',function(req,res){
-    let sql = "delete from details where id=?"
+app.get('/delete-student', function(req,res){
+    let sql = "DELETE FROM details WHERE id=?";
     let id = req.query.id;
-    con.query(sql,[id],function(error,result){
-        if (error) throw error
-
+    con.query(sql, [id], function(error, result){
+        if (error) throw error;
         res.redirect('/student');
+    });
+});
 
-
-    })
-    
-})
-
-
-app.get('/update-student',function(req,res){
-    let sql = "select * from details where id=?"
+app.get('/update-student', function(req, res){
+    let sql = "SELECT * FROM details WHERE id=?";
     let id = req.query.id;
-    con.query(sql,[id],function(error,result){
-        if (error) throw error
-        res.render(__dirname+ "/update-student",{student:result})
-        
+    con.query(sql, [id], function(error, result){
+        if (error) throw error;
+        res.render(__dirname + "/update-student", {student: result});
+    });
+});
 
-
-    })
-    
-})
-
-app.post("/update-student",function(req,res){
+app.post("/update-student", function(req, res){
     let name = req.body.name;
     let email = req.body.email;
     let contact = req.body.contact;
     let id = req.body.id;
-    //con.connect(function(error){                  shows enqueue error 
-    //   if (error) throw error
-   
-       let sql = "UPDATE details set name=?,email=?,contact=? where id = ?";
-           con.query(sql, [name, email, contact,id], function (error, result) {
-               if (error) throw error;
-               // res.send('Student successfully registered');
-               res.redirect('/student');
-       })
-    })
 
+    let sql = "UPDATE details SET name=?, email=?, contact=? WHERE id = ?";
+    con.query(sql, [name, email, contact, id], function (error, result) {
+        if (error) throw error;
+        res.redirect('/student');
+    });
+});
 
- app.get('/search-student',function(req,res){
-    let sql = "select * from details"
-    
-    con.query(sql,function(error,result){
-        if (error) throw error
-        res.render(__dirname+ "/search-student",{student:result})
-        
-    })
+app.get('/search-student', function(req, res){
+    let sql = "SELECT * FROM details";
+    con.query(sql, function(error, result){
+        if (error) throw error;
+        res.render(__dirname + "/search-student", {student: result});
+    });
+});
 
- })
-
- app.get('/search', function(req, res) {
+app.get('/search', function(req, res){
     let name = req.query.name || '';
     let email = req.query.email || '';
     let contact = req.query.contact || '';
@@ -111,12 +181,44 @@ app.post("/update-student",function(req,res){
     let sql = "SELECT * FROM details WHERE name LIKE ? AND email LIKE ? AND contact LIKE ?";
     let values = [`%${name}%`, `%${email}%`, `%${contact}%`];
 
-    con.query(sql, values, function(error, result) {
+    con.query(sql, values, function(error, result){
         if (error) throw error;
-        res.render(__dirname + "/search-student", { student: result });
+        res.render(__dirname + "/search-student", {student: result});
     });
 });
 
-app.listen(7000, function() {
-    console.log("Server is running on port 7000");
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, function() {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+//  app.get('/search-student',function(req,res){
+//     let sql = "select * from details"
+    
+//     con.query(sql,function(error,result){
+//         if (error) throw error
+//         res.render(__dirname+ "/search-student",{student:result})
+        
+//     })
+
+//  })
+
+//  app.get('/search', function(req, res) {
+//     let name = req.query.name || '';
+//     let email = req.query.email || '';
+//     let contact = req.query.contact || '';
+
+//     let sql = "SELECT * FROM details WHERE name LIKE ? AND email LIKE ? AND contact LIKE ?";
+//     let values = [`%${name}%`, `%${email}%`, `%${contact}%`];
+
+//     con.query(sql, values, function(error, result) {
+//         if (error) throw error;
+//         res.render(__dirname + "/search-student", { student: result });
+//     });
+// });
+
+// app.listen(7000, function() {
+//     console.log("Server is running on port 7000");
+// });
